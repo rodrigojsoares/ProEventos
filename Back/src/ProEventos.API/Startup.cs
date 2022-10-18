25a395ee20ab06759e2bdc158d8d48e8c64994cb
Rodrigo J. Soares <rodrigomidia@gmail.com>
2022-10-18 18:47:00 -0400

    Seção 4: Angular

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -23,6 +23,7 @@ namespace ProEventos.API
             Configuration = configuration;
         }
 
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -32,6 +33,7 @@ namespace ProEventos.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -53,6 +55,10 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+            
+            app.UseCors(x => x.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+    this.eventos =  [
+      {
+       Tema: 'Angular',
+       Local: 'Monte Negro'
+       },
+       {
+         Tema: '.NET 5',
+         Local: 'Ariquemes'
+       },
+       {
+         Tema: 'Angular e Suas Novidades',
+         Local: 'Cacoal'
+       },
+     ]
+  };
+
+}
